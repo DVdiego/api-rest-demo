97219b58a66724cf601d05407d8bba995dc07119
Diego Borja <diego-alejandro.borja-farinango.ext@siemens.com>
2023-03-12 23:21:14 +0100

    add comments
    add test task comments

--- src/test/java/com/inditex/app/controller/ApiRestControllerIntegrationTest.java
+++ src/test/java/com/inditex/app/controller/ApiRestControllerIntegrationTest.java
@@ -35,7 +35,7 @@ class ApiRestControllerIntegrationTest {
         assertEquals(4, response.getBody().size());
     }
 
-    @Test
+    @Test // Test 1: petición a las 10:00 del día 14 del producto 35455   para la brand 1 (ZARA)
     void Test1() throws ParseException {
 
         final String applyDate = "2020-06-14-10.00.00";
@@ -51,7 +51,7 @@ class ApiRestControllerIntegrationTest {
         assertEquals(1, rateBean.getBrandId());
     }
 
-    @Test
+    @Test // Test 2: petición a las 16:00 del día 14 del producto 35455   para la brand 1 (ZARA)
     void Test2() throws ParseException {
 
         final String applyDate = "2020-06-14-16.00.00";
@@ -67,7 +67,7 @@ class ApiRestControllerIntegrationTest {
         assertEquals(1, rateBean.getBrandId());
     }
 
-    @Test
+    @Test // Test 3: petición a las 21:00 del día 14 del producto 35455   para la brand 1 (ZARA)
     void Test3() throws ParseException {
 
         final String applyDate = "2020-06-14-21.00.00";
@@ -84,7 +84,7 @@ class ApiRestControllerIntegrationTest {
     }
 
 
-    @Test
+    @Test // Test 4: petición a las 10:00 del día 15 del producto 35455   para la brand 1 (ZARA)
     void Test4() throws Exception {
 
         final String applyDate = "2020-06-15-10.00.00";
@@ -102,7 +102,7 @@ class ApiRestControllerIntegrationTest {
 
     }
 
-    @Test
+    @Test // Test 5: petición a las 21:00 del día 16 del producto 35455   para la brand 1 (ZARA)
     void Test5() throws ParseException {
 
         final String applyDate = "2020-06-16-21.00.00";
