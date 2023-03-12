7eeaed8471d0792aebe5406e9c94f42a6bfb3d41
Diego Borja <diego-alejandro.borja-farinango.ext@siemens.com>
2023-03-12 22:10:02 +0100

    Create RunService.java
    run service

---
+++ src/main/java/com/inditex/app/RunService.java
@@ -0,0 +1,14 @@
+package com.inditex.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+public class RunService {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RunService.class, args);
+	}
+
+}
