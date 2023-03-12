cf910d80b26d06f8241c6e1a2f2447ab719c2e8e
Diego Borja <diego-alejandro.borja-farinango.ext@siemens.com>
2023-03-12 23:17:13 +0100

    clean code

--- src/main/java/com/inditex/app/model/PriceBean.java
+++ src/main/java/com/inditex/app/model/PriceBean.java
@@ -3,10 +3,8 @@ package com.inditex.app.model;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.io.Serializable;
--- src/main/java/com/inditex/app/repository/IRepository.java
+++ src/main/java/com/inditex/app/repository/IRepository.java
@@ -13,15 +13,6 @@ import java.util.List;
 @Repository
 public interface IRepository extends JpaRepository<PriceBean, Long>  {
 
-/*    @Query(value = "SELECT * FROM PRICES p " +
-            " WHERE p.start_date <= :applyDate" +
-            " and p.end_date >= :applyDate" +
-            " and p.product_id = :productId" +
-            " and p.brand_id = :brandId" +
-            " ORDER BY p.priority DESC"
-            //to get just one -> " LIMIT 1"
-            , nativeQuery = true)
-    PriceBean save(PriceBean priceBean);*/
     @Query(value = "SELECT * FROM PRICES p " +
                     " WHERE p.start_date <= :applyDate" +
                         " and p.end_date >= :applyDate" +
